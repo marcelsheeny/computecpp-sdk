cba57519b186d01ae57772af083efac724f9645d
Marcel Sheeny <marcelsheeny@gmail.com>
2018-03-16 17:44:38 +0000

    adding sycl_utils.hpp

---
+++ demos/include/sycl_utils.hpp
@@ -0,0 +1,39 @@
+/***************************************************************************
+ *
+ *  Copyright (C) 2018 Codeplay Software Limited
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  For your convenience, a copy of the License has been included in this
+ *  repository.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Codeplay's ComputeCpp SDK
+ *
+ *  sycl_utils.hpp
+ *
+ *  Description:
+ *    Utilities functions for SYCL.
+ *
+ **************************************************************************/
+
+#pragma once
+#include <CL/sycl.hpp>
+
+auto sycl_exception_handler = [](cl::sycl::exception_list el) {
+  for (auto e : el) {
+    try {
+      std::rethrow_exception(e);
+    } catch (std::exception const& e) {
+      std::cout << "Caught SYCL exception:\n" << e.what() << std::endl;
+    }
+  }
+};
